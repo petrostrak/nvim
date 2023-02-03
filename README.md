bdf755760ff6bdf8c95eeebe46b1f363630dfc81
petrostrak <pit.trak@gmail.com>
2023-02-03 18:32:58 +0200

    Updated README

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ To full utilize nvim-telescope, the ripgre binary is needed
 ```
 brew install ripgrep
 ```
+
+In case nvim is missing from the python3 directories
+```
+python3 -m pip install --user --upgrade pynvim
+```
+
+To install new plugins in nvim
+
+* `:w` 
+* `:luafile %` 
+* `:PackerInstall`
--- lua/plugins/default.lua
+++ lua/plugins/default.lua
@@ -1,23 +1,6 @@
 -- vim.g.mapleader = ","
 vim.g.mapleader = " "
 
--- vim.opt.encoding="utf-8"
--- vim.opt.compatible=false
--- vim.opt.hlsearch=true
--- vim.opt.relativenumber = true
--- vim.opt.laststatus = 2
--- vim.opt.vb = true
--- vim.opt.ruler = true
--- vim.opt.spelllang="en_us"
--- vim.opt.autoindent=true
--- vim.opt.colorcolumn="120"
--- vim.opt.textwidth=120
--- vim.opt.mouse="a"
--- vim.opt.clipboard="unnamed"
--- vim.opt.scrollbind=false
--- vim.opt.wildmenu=true
-
-
 -- line numbers
 vim.opt.relativenumber = true -- show relative line numbers
 vim.opt.number = true -- shows absolute line number on cursor line (when relative number is on)
