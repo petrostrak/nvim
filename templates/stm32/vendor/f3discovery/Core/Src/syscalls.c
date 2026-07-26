1be317e0dfcbfb0b8f3e990341549b400315de09
petrostrak <pit.trak@gmail.com>
2026-07-26 10:52:09 +0300

    add ITM_SendChar

--- templates/stm32/vendor/f3discovery/Core/Src/syscalls.c
+++ templates/stm32/vendor/f3discovery/Core/Src/syscalls.c
@@ -18,6 +18,14 @@
  * If no LICENSE file comes with this software, it is provided AS-IS.
  *
  ******************************************************************************
+ * @function  void ITM_SendChar(uint8_t ch);
+ * @author    FastBit Embedded Brain Academy
+ * @brief 	  Implementation of printf like feature using ARM Cortex 
+ *			  M3/M4/ ITM functionality
+ *			  This function will not work for ARM Cortex M0/M0+
+ *			  If you are using Cortex M0, then you can use semihosting 
+ *			  feature of openOCD
+ ******************************************************************************
  */
 
 /* Includes */
@@ -30,6 +38,13 @@
 #include <sys/time.h>
 #include <sys/times.h>
 
+/* Definitions */
+//Debug Exception and Monitor Control Register base address
+#define DEMCR        			*((volatile uint32_t*) 0xE000EDFCU )
+
+/* ITM register addresses */
+#define ITM_STIMULUS_PORT0   	*((volatile uint32_t*) 0xE0000000 )
+#define ITM_TRACE_EN          	*((volatile uint32_t*) 0xE0000E00 )
 
 /* Variables */
 extern int __io_putchar(int ch) __attribute__((weak));
@@ -41,6 +56,22 @@ char **environ = __env;
 
 
 /* Functions */
+void ITM_SendChar(uint8_t ch)
+{
+
+	//Enable TRCENA
+	DEMCR |= ( 1 << 24);
+
+	//enable stimulus port 0
+	ITM_TRACE_EN |= ( 1 << 0);
+
+	// read FIFO status in bit [0]:
+	while(!(ITM_STIMULUS_PORT0 & 1));
+
+	//Write to ITM stimulus port0
+	ITM_STIMULUS_PORT0 = ch;
+}
+
 void initialise_monitor_handles()
 {
 }
